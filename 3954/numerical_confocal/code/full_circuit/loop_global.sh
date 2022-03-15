5923f6cb7eb5efbf184e5216a0684abb603cfa5c
Martina Oliver <martinaoliver1@gmail.com>
2022-03-15 15:08:35 +0000

    holis

--- 3954/numerical_confocal/code/full_circuit/loop_global.sh
+++ 3954/numerical_confocal/code/full_circuit/loop_global.sh
@@ -4,11 +4,11 @@
 #  done
 
 
-for i in $(seq 1 1000 10000)
+#for i in $(seq 1 1000 10000)
 # for i in $(seq 1 1000 30000)
-# for i in $(seq 0 1 0)
+for i in $(seq 0 1 0)
 do
-  # qsub -v start=$i,end=$(($i+1)) global.pbs
-  qsub -v start=$i,end=$(($i+1000)) global.pbs
+  qsub -v start=$i,end=$(($i+1)) global.pbs
+  #qsub -v start=$i,end=$(($i+1000)) global.pbs
    echo "Welcome $i times and $(($i+1))"
 done
