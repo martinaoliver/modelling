38299b612c21da5f96f21c0126d3f2882ff7e41b
martinaoliver <martinaoliver1@gmail.com>
2022-03-15 14:31:18 +0000

    changes in plotting

--- 3954/numerical_confocal/code/full_circuit/sample_general_numerical.py
+++ 3954/numerical_confocal/code/full_circuit/sample_general_numerical.py
@@ -5,7 +5,7 @@ import sys
 import os
 pwd = os.getcwd()
 root = pwd.rpartition("mo2016")[0] + pwd.rpartition("mo2016")[1] #/Volumes/mo2016/ or '/Users/mo2016/' or '/rds/general/mo2016/'
-
+print(root)
 if root == '/Users/mo2016':
     modelling_ephemeral = '/Volumes/mo2016/ephemeral/Documents/modelling'
     modelling_home = '/Volumes/mo2016/home/Documents/modelling'
@@ -33,6 +33,7 @@ shape = 'square' #'ca'
 parID = int(sys.argv[1])
 circuit_n=2
 variant='5716gaussian'
+folder='fullcircuit_5716gaussian'
 parametersets_n = 1000 #1000000
 save_figure = True
 tqdm_disable = False #disable tqdm
@@ -54,11 +55,13 @@ D[1]=d_B
 #solver parameters
 L_x=int(sys.argv[2]); x_gridpoints = int(sys.argv[3]); J = L_x*x_gridpoints;  L_y=L_x; I=J
 T =int(sys.argv[4]); t_gridpoints = int(sys.argv[5]) ; N = T*t_gridpoints
+suggested_tgridpoints = x_gridpoints**2
+
 filename = 'circuit%r_variant%s_%s_%sID%r_L%r_J%r_T%r_N%r'%(circuit_n,variant, shape,mechanism,parID,L_x,J,T,N)#,p_division,kce)
-savefig_path = modelling_ephemeral + '/3954/numerical_confocal/results/figures/1M_colony_ca/2D/nodeA_dele_newCN'
+savefig_path = modelling_ephemeral + '/3954/numerical_confocal/results/figures/square/%s'%folder
 try:
     U_record,U_final = adi(par_dict,L_x,L_y,J,I,T,N, circuit_n,n_species,D, tqdm_disable=tqdm_disable)#,p_division=p_division,seed=seed)
-    plot_2D_final_concentration(U_final,L_x,J,filename,n_species=n_species)
+    plot_2D_final_concentration(U_final,L_x,J,filename,savefig_path,n_species=n_species,save_figure=True)
         # savefig_path = modelling_ephemeral + '/3954/numerical_confocal/results/figures/1M_colony_ca/2D/5716gaussian'
     # plot_redgreen_contrast(U_final,L_x,mechanism,shape,filename,savefig_path,parID=parID,scale_factor=x_gridpoints,save_figure=save_figure)
     # plot_redgreen_contrast(U_final,L_x,mechanism,shape,filename,savefig_path,parID=parID,scale_factor=x_gridpoints,save_figure=False)
