2952c01fe3cbeb603bf9c02e661017431ff1e111
Martina Oliver <martinaoliver1@gmail.com>
2022-05-18 17:24:58 +0100

    big library search

--- 3954/parameter_space_search/parameterfiles/print_df.py
+++ 3954/parameter_space_search/parameterfiles/print_df.py
@@ -5,7 +5,10 @@ circuit=2
 variant=0
 n_parametersets = 1000000
 n_parametersets = 10000
-df = pd.read_pickle("df_circuit%r_variant%r_%rparametersets.pkl"%(circuit,variant,n_parametersets))
+# df = pd.read_pickle("df_circuit%r_variant%r_%rparametersets.pkl"%(circuit,variant,n_parametersets))
+df = pd.read_pickle('df_circuit2_variant1_1954parametersets_rbslibrary0.pkl')
+
 print('g')
 # pickle.dump( df.loc[0:20000], open( "df_circuit%r_variant%r_%rparametersets.pkl"%(circuit,variant,10000), "wb" ) )
-print(df.loc[5716])
+# print(df.loc[5716])
+print(df)
