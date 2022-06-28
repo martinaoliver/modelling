cd616333635bd0b6365b5c3d1a0e3dd5ce869405
martinaoliver <martinaoliver1@gmail.com>
2022-06-28 12:04:02 +0200

    masks created for roozbeh and redgreenblue plotted

--- 3954/modules/new_CN/plotting_numerical.py
+++ 3954/modules/new_CN/plotting_numerical.py
@@ -134,6 +134,46 @@ def plot_redgreen_contrast(final_concentration, mm, mechanism, shape, filename, 
 
     return rgb
 
+def plot_redgreenblue_contrast(final_concentration, mm, mechanism, shape, filename, path, parID=0, scale_factor=10, save_figure=False, dimension='2D'):
+    green = final_concentration[-1]
+    red = final_concentration[-2]
+    blue = final_concentration[0]
+    x_grid = np.linspace(0, mm, len(green))
+    normalised_red, redmin, redmax = matrix_rgb_normalisation(red)
+    normalised_green, greenmin, greenmax = matrix_rgb_normalisation(green)
+    normalised_blue, bluemin, bluemax = matrix_rgb_normalisation(blue)
+
+    zeros = np.zeros(normalised_green.shape)
+    rgb = np.dstack((normalised_red, normalised_green, normalised_blue))
+    rgb = np.rot90(rgb)
+    if save_figure != 'LargeImage':
+        plt.imshow(rgb.astype('uint8'), origin='lower')
+        tick_positions = np.arange(0, len(normalised_green), len(normalised_green) / 4)
+        tick_labels = np.arange(0, len(normalised_green) / scale_factor,
+                                len(normalised_green) / scale_factor / 4).round(decimals=2)
+        plt.xticks(tick_positions, tick_labels)
+        plt.yticks(tick_positions, tick_labels)
+        plt.ylabel('y axis (mm)', size=16)
+        plt.xlabel('x axis (mm)', size=16)
+        plt.yticks(size=15)
+        plt.xticks(size=15)
+        plt.title('parID=' + str(parID), size=14)
+        np.set_printoptions(precision=2)
+        plt.text(1,1,'mCherry = [%r-%r]'%(np.around(redmin,2),np.around(redmax,2)),c='r')
+        plt.text(1,5,'GPF = [%r-%r]'%(np.around(greenmin,2),np.around(greenmax,2)),c='g')
+        plt.text(1,10,'GPF = [%r-%r]'%(np.around(bluemin,2),np.around(bluemax,2)),c='g')
+        plt.tight_layout()
+
+        if save_figure == True:
+            plt.savefig(path + '/%s_%s.jpeg' % (dimension, filename),dpi=2000)
+            plt.close()
+        else:
+            plt.show()
+
+
+    return rgb
+
+
 
 #video
 def redgreen_contrast_timeseries(records):
