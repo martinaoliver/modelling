8388a31faf0a9964ddd7ad40b0aedcc71a8a44aa
Martina Oliver <martinaoliver1@gmail.com>
2022-08-01 18:20:08 +0100

    lsa multithread adapted for growth

--- growth/src/analytical/run_lsa_multithread.py
+++ growth/src/analytical/run_lsa_multithread.py
@@ -35,8 +35,8 @@ else:
 print('Number of Threads set to ', Number_of_Threads)
 # Number_of_Threads=48
 # Specify name of circuit and variant investigated
-circuit_n=2
-variant= 9
+circuit_n='schnakenberg'
+variant= 0
 
 # Specifiy number of parameter sets in parameterset file to be loaded
 df_lenght = 10
@@ -63,7 +63,7 @@ def lsa_check(start_batch_index,n_param_sets,df,circuit_n='turinghill', variant=
     print('pool' + str(start_batch_index))
     output_df = big_turing_analysis_df(df,circuit_n,n_species,print_parID=False)
     print('calculated')
-    pickle.dump(output_df, open(modellingpath + '/growth/out/lsa_dataframes/lsa_df_%r_variant%r_%rparametersets_batch%r.pkl'%(circuit_n,variant,n_param_sets,start_batch_index), 'wb'))
+    pickle.dump(output_df, open(modellingpath + '/growth/out/analytical/lsa_dataframes/lsa_df_%s_variant%r_%rparametersets_batch%r.pkl'%(circuit_n,variant,n_param_sets,start_batch_index), 'wb'))
     # pickle.dump(output_df, open('../results/output_dataframes/lsa_df_circuit%r_variant%r_%rparametersets_batch%r_rbslibrary0.pkl'%(circuit_n,variant,n_param_sets,start_batch_index), 'wb'))
     print('saved')
 # Runs if the module is the main program
@@ -74,7 +74,7 @@ start_parameter = int(0)
 # Load dataframe of parameter sets
 print('df_%r_variant%r_%rparametersets.pkl'%(circuit_n,variant,n_param_sets))
 # df= pickle.load( open('../parameterfiles/df_circuit%r_variant%r_%rparametersets.pkl'%(circuit_n,variant,n_param_sets), "rb" ) )
-df= pickle.load( open(modellingpath + '/growth/out/lsa_dataframes/lsa_df_%r_variant%s_%rparametersets.pkl'%(circuit_n,variant,n_param_sets), "rb" ) )
+df= pickle.load( open(modellingpath + '/growth/out/analytical/lsa_dataframes/lsa_df_%s_variant%s_%rparametersets.pkl'%(circuit_n,variant,n_param_sets), "rb" ) )
 # df= pickle.load( open("../parameterfiles/df_circuit2_variant1_1954parametersets_rbslibrary0.pkl", "rb"))
 batch_indices = list(range(0+start_parameter, len(df) + start_parameter, batch_size))
 # batch_indices = list(range(0+start_parameter, 10 + start_parameter, batch_size))
@@ -110,7 +110,7 @@ my_data = {}
 
 # Load all batch dataframes
 for start_batch_index in batch_indices:
-    my_data[start_batch_index] = pickle.load(open(modellingpath + '/growth/out/lsa_dataframes/lsa_df_%r_variant%r_%rparametersets_batch%r.pkl'%(circuit_n,variant,n_param_sets,start_batch_index), "rb" ) )
+    my_data[start_batch_index] = pickle.load(open(modellingpath + '/growth/out/analytical/lsa_dataframes/lsa_df_%s_variant%r_%rparametersets_batch%r.pkl'%(circuit_n,variant,n_param_sets,start_batch_index), "rb" ) )
     # my_data[start_batch_index] = pickle.load(open('../results/output_dataframes/lsa_df_circuit%r_variant%r_%rparametersets_batch%r_rbslibrary0.pkl'%(circuit_n,variant,n_param_sets,start_batch_index), "rb" ) )
 # Join all batch results to large results dataframe
 results_df = pd.concat(my_data.values(), ignore_index=False)
@@ -119,4 +119,4 @@ results_df = pd.concat(my_data.values(), ignore_index=False)
 tupled_index =  [tuple(l) for l in results_df.index]
 multi_index = pd.MultiIndex.from_tuples(tupled_index)
 results_df = results_df.set_index(multi_index)
-pickle.dump(results_df, open(modellingpath + '/growth/out/lsa_dataframes/lsa_df_%r_variant%r_%rparametersets.pkl'%(circuit_n,variant,n_param_sets), 'wb'))
+pickle.dump(results_df, open(modellingpath + '/growth/out/analytical/lsa_dataframes/lsa_df_%s_variant%r_%rparametersets.pkl'%(circuit_n,variant,n_param_sets), 'wb'))
