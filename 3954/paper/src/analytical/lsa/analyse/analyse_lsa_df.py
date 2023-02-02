0c3153d07d71d1a61ef7cd5983a821a43f2ef6eb
Martina Oliver <martinaoliver1@gmail.com>
2023-02-02 16:06:41 +0000

    confusion matrix balancing and analyse_df

--- 3954/paper/src/analytical/lsa/analyse/analyse_lsa_df.py
+++ 3954/paper/src/analytical/lsa/analyse/analyse_lsa_df.py
@@ -1,3 +1,4 @@
+#%%
 #############
 ###paths#####
 #############
@@ -16,37 +17,41 @@ import numpy as np
 
 
 def pieChart_lsa(valueCounts_dict,title,log=True):
-    colors=['grey','grey','grey','grey','peachpuff','coral','coral','coral','coral','coral','grey','coral','coral']
-    colors=['grey','grey','grey','grey','peachpuff','peachpuff','peachpuff','coral','coral','coral']
+    colors_dict={'simple stable':'grey','simple unstable':'grey','complex unstable':'grey','no steady state':'grey','hopf':'peachpuff','turing I hopf':'peachpuff','turing I':'coral','turing I oscillatory':'coral'}
+    # colors=['grey','grey','grey','grey','peachpuff','peachpuff','peachpuff','coral','coral','coral']
     labels = []
     sizes = []
-    
+    colors= []
     for x, y in valueCounts_dict.items():
         labels.append(x)
         sizes.append(y)
+        colors.append(colors_dict[x])
     if log==True:
         sizes = np.log(sizes)
     plt.pie(sizes,colors=colors, labels=labels)
     plt.axis('equal')
     plt.title(title)
     plt.show()
-
+#%%
 
 # Specify name of circuit and variant investigated
 circuit_n='circuit14'
 variant='1nd'
 # Specifiy number of parameter sets in parameterset file to be loaded
-n_param_sets = 5000000
+n_param_sets = 1000000
 
 print(f'Circuit:{circuit_n}, Variant:{variant}')
 
-df= pickle.load( open(modellingpath + '/3954/paper/out/analytical/lsa_dataframes/all_dataframes/lsa_df_%s_variant%s_%rparametersets_balanced.pkl'%(circuit_n,variant,n_param_sets), "rb"))
+df= pickle.load( open(modellingpath + '/3954/paper/out/analytical/lsa_dataframes/all_dataframes/lsa_df_%s_variant%s_%rparametersets_notBalanced.pkl'%(circuit_n,variant,n_param_sets), "rb"))
 print(df['system_class'].value_counts())
+
+# %%
+
 valueCounts_dict = dict(df['system_class'].value_counts())
-title = f'{circuit_n} Variant {variant}'
+title = f'{circuit_n} Variant {variant} Not-Balanced'
 pieChart_lsa(valueCounts_dict,title)
 dfunstable = df[df['system_class']=='simple unstable']
-
+#%%
 if balanced == True:
     
 
