737008a080fa4113defb63212083499fc9d75e86
Martina Oliver Huidobro <mo2016@Martinas-Laptop.local>
2023-03-13 14:21:50 +0000

    automatic creation of masks

--- 3954/paper/src/numerical/colonies/colonyMaskCreation.py
+++ 3954/paper/src/numerical/colonies/colonyMaskCreation.py
@@ -51,7 +51,7 @@ def cell_automata_colony(cell_matrix, p_division):
     return cell_matrix_new, daughterToMotherDict
     
 # numba.jit(nopython=True)
-def adi_ca(L,dx,J,T,dt,N,n_species,division_TimeHours,tqdm_disable=False, p_division=0.3,stochasticity=0, seed=1, growth='Fast'):
+def adi_ca(L,dx,J,T,dt,N,n_species,divisionTimeHours,tqdm_disable=False, p_division=0.3,stochasticity=0, seed=1, growth='Fast'):
     I=J
 
     print(f'dt{dt}')
@@ -111,100 +111,109 @@ def adi_ca(L,dx,J,T,dt,N,n_species,division_TimeHours,tqdm_disable=False, p_divi
     # print(np.shape(cell_matrix_record))
     return cell_matrix_record,memory_matrix_record, daughterToMotherDictList
 
-#execution parameters
+def maskFunction(L=9, dx=0.05, T=50, dt=0.05, divisionTimeHours=1, p_division=0.5,seed=1,plot1D=True, plotScatter = True, plotVideo=False):
+        
+    #execution parameters
 
-save_figure = False
-tqdm_disable = False #disable tqdm
-n_species=6
-#%%
-# L=5; x_gridpoints =5; J = L*x_gridpoints
-# T =10; t_gridpoints = 10; N = T*t_gridpoints
-# L=4; dx =0.025; J = int(L/dx)
-# T =65; dt = 0.0025; N = int(T/dt)
-L=10; dx =0.05; J = int(L/dx)
-T =150; dt = 0.05; N = int(T/dt)
+    save_figure = False
+    tqdm_disable = False #disable tqdm
+    n_species=6
+    #%%
+    # L=5; x_gridpoints =5; J = L*x_gridpoints
+    # T =10; t_gridpoints = 10; N = T*t_gridpoints
+    # L=4; dx =0.025; J = int(L/dx)
+    # T =65; dt = 0.0025; N = int(T/dt)
+    # L=10; dx =0.05; J = int(L/dx)
+    # T =150; dt = 0.05; N = int(T/dt)
 
-L=10; dx =0.05; J = int(L/dx)
-T =150; dt = 1; N = int(T/dt)
-boundarycoeff = 1
+    J = int(L/dx)
+    N = int(T/dt)
+    # boundarycoeff = 1
 
-divisionTimeHours=1
-p_division=0.5;seed=1
+    # divisionTimeHours=1
+    # p_division=0.5;seed=1
 
-# L=8; dx =0.05; J = int(L/dx)
-# T =50; dt = 0.05; N = int(T/dt)
-# boundarycoeff = 1
+    # L=9; dx =0.05; J = int(L/dx)
+    # T =50; dt = 0.05; N = int(T/dt)
+    # boundarycoeff = 1
 
-# divisionTimeHours=1
-# p_division=0.22;seed=1
+    # divisionTimeHours=0.5
+    # p_division=1;seed=1
 
-# L=int(sys.argv[1]); dx =float(sys.argv[2]); J = int(L/dx)
-# T =int(sys.argv[3]); dt = float(sys.argv[4]); N = int(T/dt)
+    # L=8; dx =0.05; J = int(L/dx)
+    # T =50; dt = 0.05; N = int(T/dt)
+    # boundarycoeff = 1
 
-L_x = L
-L_y = L
-I = J
+    # divisionTimeHours=1
+    # p_division=0.22;seed=1
 
-# p_division=float(sys.argv[5]);seed=1
+    # L=int(sys.argv[1]); dx =float(sys.argv[2]); J = int(L/dx)
+    # T =int(sys.argv[3]); dt = float(sys.argv[4]); N = int(T/dt)
 
-cell_matrix_record,memory_matrix_record, daughterToMotherDictList = adi_ca(L,dx,J,T,dt,N,n_species,divisionTimeHours,tqdm_disable=False,p_division=p_division,seed=seed)
-pickle.dump( cell_matrix_record, open(modellingpath + "/3954/paper/out/numerical/masks/caMask_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "wb" ) )
-pickle.dump( memory_matrix_record, open(modellingpath + "/3954/paper/out/numerical/masks/caMemory_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "wb" ) )
-pickle.dump( daughterToMotherDictList, open(modellingpath + "/3954/paper/out/numerical/masks/caMemory_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "wb" ) )
+    L_x = L
+    L_y = L
+    I = J
 
-# print(np.shape(cell_matrix_record))
-# for ti in range(N):
-    # print(np.shape(memory_matrix_record[:,:,ti]))
-    # print(memory_matrix_record[:,:,ti])
-#%%
-plot1D=True
-if plot1D == True:
-    print('Final Mask')
-    plt.imshow(cell_matrix_record[:,:,-1], cmap='Greys')# plot_2D_final_concentration(final_concentration,grids,filename,n_species=n_species)
-    tick_positions = np.linspace(0, L/dx, 4)
-    tick_labels = np.linspace(0, L, 4).round(decimals=2)
-    plt.xticks(tick_positions, tick_labels)
-    plt.yticks(tick_positions, tick_labels)
-    plt.savefig(modellingpath + "/3954/paper/out/numerical/masks/caMask_seed%s_pdivision%s_L%s_J%s_T%s_N%s.png"%(seed,p_division,L,J,T,N))
-    plt.show()
-    plt.close()
-#%%
-plotVideo=False
-if plotVideo==True:
-    def show_rgbvideo(timeseries_unstacked):
-        time=0
+    # p_division=float(sys.argv[5]);seed=1
 
-        fig = plt.plot()
-        rgb_timeseries=timeseries_unstacked # Read the numpy matrix with images in the rows
-        N = len(rgb_timeseries)
-        im=plt.imshow(rgb_timeseries[:,:,0].astype('uint8'), origin= 'lower', cmap='Greys')
-        for time in range(len(rgb_timeseries[0,0,:])):
-            im.set_data(rgb_timeseries[:,:,time].astype('uint8'))
-            
-            plt.xlabel(time)
-            plt.pause(0.00000000000000001)
+    cell_matrix_record,memory_matrix_record, daughterToMotherDictList = adi_ca(L,dx,J,T,dt,N,n_species,divisionTimeHours,tqdm_disable=False,p_division=p_division,seed=seed)
+    pickle.dump( cell_matrix_record, open(modellingpath + "/3954/paper/out/numerical/masks/caMask_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "wb" ) )
+    pickle.dump( memory_matrix_record, open(modellingpath + "/3954/paper/out/numerical/masks/caMemory_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "wb" ) )
+    pickle.dump( daughterToMotherDictList, open(modellingpath + "/3954/paper/out/numerical/masks/caMemory_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "wb" ) )
+
+    # print(np.shape(cell_matrix_record))
+    # for ti in range(N):
+        # print(np.shape(memory_matrix_record[:,:,ti]))
+        # print(memory_matrix_record[:,:,ti])
+    #%%
+    if plot1D == True:
+        print('Final Mask')
+        plt.imshow(cell_matrix_record[:,:,-1], cmap='Greys')# plot_2D_final_concentration(final_concentration,grids,filename,n_species=n_species)
+        tick_positions = np.linspace(0, L/dx, 4)
+        tick_labels = np.linspace(0, L, 4).round(decimals=2)
+        plt.xticks(tick_positions, tick_labels)
+        plt.yticks(tick_positions, tick_labels)
+        plt.savefig(modellingpath + "/3954/paper/out/numerical/masks/caMask_seed%s_pdivision%s_L%s_J%s_T%s_N%s.png"%(seed,p_division,L,J,T,N))
+        plt.show()
+        plt.close()
+    #%%
+    if plotVideo==True:
+        def show_rgbvideo(timeseries_unstacked):
+            time=0
+
+            fig = plt.plot()
+            rgb_timeseries=timeseries_unstacked # Read the numpy matrix with images in the rows
+            N = len(rgb_timeseries)
+            im=plt.imshow(rgb_timeseries[:,:,0].astype('uint8'), origin= 'lower', cmap='Greys')
+            for time in range(len(rgb_timeseries[0,0,:])):
+                im.set_data(rgb_timeseries[:,:,time].astype('uint8'))
+                
+                plt.xlabel(time)
+                plt.pause(0.00000000000000001)
+            plt.show()
+
+        show_rgbvideo(cell_matrix_record)
+
+    #%%
+    if plotScatter==True:
+        print('Scatter')
+        lenght_list = []
+        for i in range(len(cell_matrix_record[0,0,:])):
+            lenght = np.count_nonzero(cell_matrix_record[:,int(J/2),i])
+            lenght_list.append(lenght)
+        lenght_list = np.array(lenght_list)*dx
+
+        plt.scatter(range(0,N),lenght_list, c='k',s=1)
+        plt.xlabel('Time (hours)')
+        plt.ylabel('Colony diameter (mm)')
+        tick_positions = np.linspace(0, T/dt, 4)
+        tick_labels = np.linspace(0, T , 4).round(decimals=2)
+        plt.xticks(tick_positions, tick_labels)
+        plt.savefig(modellingpath + "/3954/paper/out/numerical/masks/growthScatter_seed%s_pdivision%s_L%s_J%s_T%s_N%s.png"%(seed,p_division,L,J,T,N))
         plt.show()
 
-    show_rgbvideo(cell_matrix_record)
 
-#%%
-plotScatter = True
-if plotScatter==True:
-    print('Scatter')
-    lenght_list = []
-    for i in range(len(cell_matrix_record[0,0,:])):
-        lenght = np.count_nonzero(cell_matrix_record[:,int(J/2),i])
-        lenght_list.append(lenght)
-    lenght_list = np.array(lenght_list)*dx
-
-    plt.scatter(range(0,N),lenght_list, c='k',s=1)
-    plt.xlabel('Time (hours)')
-    plt.ylabel('Colony diameter (mm)')
-    tick_positions = np.linspace(0, T/dt, 4)
-    tick_labels = np.linspace(0, T , 4).round(decimals=2)
-    plt.xticks(tick_positions, tick_labels)
-    plt.savefig(modellingpath + "/3954/paper/out/numerical/masks/growthScatter_seed%s_pdivision%s_L%s_J%s_T%s_N%s.png"%(seed,p_division,L,J,T,N))
-    plt.show()
-
-
+    # %%
+    
+# maskFunction(L=6,dx=0.05, T=50, dt=0.05, divisionTimeHours=0.5, p_division=0.5)
 # %%
--- 3954/paper/src/numerical/colonies/run_numerical_single.py
+++ 3954/paper/src/numerical/colonies/run_numerical_single.py
@@ -15,6 +15,8 @@ import time
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+
 #############
 ###Imports#####
 #############
@@ -27,8 +29,10 @@ from numerical.adi_ca_function_openclosed_nodilution_preMask_numba import \
     adi_ca_openclosed_nodilution_preMask_numba
 from numerical.adi_square_function import adi
 from numerical.plotting_numerical import *
-
-# %matplotlib inline
+from numerical.cn_plot import *
+from colonyMaskCreation import *
+#%%
+# # %matplotlib inline
 #############
 ###execution parameters#####
 #############
@@ -56,36 +60,45 @@ with open(modellingpath + '/3954/paper/out/analytical/lsa_dataframes/turing_data
 print(df)
 #solver parameters
 # specify dimensions of system
-L=9; dx =0.05; J = int(L/dx)
-T =50; dt = 0.05; N = int(T/dt)
+L=4; dx =0.05; J = int(L/dx)
+T =50; dt = 0.005; N = int(T/dt)
 boundarycoeff = 1
 
 divisionTimeHours=0.5
-p_division=1;seed=1
-
+p_division=0.3;seed=1
+shape = 'ca'
+x_gridpoints=int(1/dx)
 # divisionTimeHours=0.5
 # p_division=0.22;seed=1
 # L=int(sys.argv[1]); dx =float(sys.argv[2]); J = int(L/dx)
 # T =int(sys.argv[3]); dt = float(sys.argv[4]); N = int(T/dt)
 
+try:
+    cell_matrix_record = pickle.load( open(modellingpath + "/3954/paper/out/numerical/masks/caMask_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "rb" ) )
+    daughterToMotherDictList = pickle.load( open(modellingpath + "/3954/paper/out/numerical/masks/caMemory_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "rb" ) )
+except:
+    #file does not exist
+    FileNotFoundError
+    maskFunction(L=L,dx=dx, T=T, dt=dt, divisionTimeHours=divisionTimeHours, p_division=p_division, plot1D=True, plotScatter=True)
+    cell_matrix_record = pickle.load( open(modellingpath + "/3954/paper/out/numerical/masks/caMask_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "rb" ) )
+    daughterToMotherDictList = pickle.load( open(modellingpath + "/3954/paper/out/numerical/masks/caMemory_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "rb" ) )
 
-cell_matrix_record = pickle.load( open(modellingpath + "/3954/paper/out/numerical/masks/caMask_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "rb" ) )
-daughterToMotherDictList = pickle.load( open(modellingpath + "/3954/paper/out/numerical/masks/caMemory_seed%s_pdivision%s_L%s_J%s_T%s_N%s.pkl"%(seed,p_division,L,J,T,N), "rb" ) )
-# T =1; dt = 0.05; N = int(T/dt)
+    
+    # T =1; dt = 0.05; N = int(T/dt)
 # 0.5,0.02, 0.005
 # T =2; dt = 0.05; N = int(T/dt)
 filename= lambda parID: 'circuit%r_variant%s_bc%s_%s_ID%r_L%r_J%r_T%r_N%r'%(circuit_n,variant,boundarycoeff, shape,parID,L,J,T,N)
 #%%
+
 parID=316386
-# for parID in range(10):
-par_dict = df.iloc[0].to_dict()
+print('parID = ' + str(parID))
+par_dict = df.loc[parID].to_dict()
 D = np.zeros(n_species)
 Dr = float(par_dict['Dr'])
+D[:2] = [1,Dr ]
 degDiv = 1
 par_dict['muASV'] =par_dict['muASV']/degDiv
 par_dict['muLVA'] = par_dict['muLVA'] /degDiv
-D[:2] = [1,Dr ]
-
 print(par_dict)
 
 # U_record,U_final =  adi_ca_openclosed_nodilution_preMask(par_dict,L,dx,J,T,dt,N, circuit_n, n_species,D,cell_matrix_record, daughterToMotherDictList,tqdm_disable=False, p_division=0.5,stochasticity=0, seed=1,growth='Slow', boundarycoeff=boundarycoeff)
@@ -105,4 +118,10 @@ pickle.dump(U_record, open(modellingpath + '/3954/paper/out/numerical/colonies/s
 
 print(modellingpath + '/3954/paper/out/numerical/colonies/simulation/%s/2Drecord_%s_degDiv%s.pkl'%(folder,filename(parID), degDiv))
 print('saved')
+# %%
+rgb = plot_redgreen_contrast(U_final,L,parID=parID,scale_factor=x_gridpoints,save_figure=False)
+
+# %%
+plt.imshow(U_final[-1])
+plt.colorbar()
 # %%
